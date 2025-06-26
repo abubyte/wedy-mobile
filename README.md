0a9f0e88c7350592af896e20d3ca64fe995faa92
abdurrohmandavron <abdurakhmon278@gmail.com>
2025-06-26 19:41:30 +0500

    Bismillah

---
+++ README.md
@@ -0,0 +1,6 @@
+# wedy
+
+Versions:
+Flutter 3.32.5
+Dart 3.8.1
+DevTools 2.45.1
---
+++ android/app/src/main/kotlin/uz/abubyte/wedy/MainActivity.kt
@@ -0,0 +1,5 @@
+package uz.abubyte.wedy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
